bf62ba71eeb17019ad965fa1735823d616100819
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-16 13:19:55 -0700

    Create user model tests. Add bcrypt gem to gemfile
    Co-Authored-By: null <LainMcGrath@users.noreply.github.com>
    Co-Authored-By: Rachel Lew <rlew421@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates_presence_of :name, :address, :city, :state, :zip, :password, :password_confirmation
+  validates :email, uniqueness: true, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  has_secure_password
 end
