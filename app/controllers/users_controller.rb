dca83eb31105b8e300fd92e4470293928bf8181e
Dylan Connolly <dylanconnolly145@gmail.com>
2019-12-16 13:20:31 -0700

    Add create action for new user
    Co-Authored-By: null <LainMcGrath@users.noreply.github.com>
    Co-Authored-By: Rachel Lew <rlew421@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,18 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+
+    if user.save
+      flash[:success] = "Account created!"
+      redirect_to "/profile"
+    end
+    flash[:error] = "Incomplete form."
+    require "pry"; binding.pry
   end
 
   private
 
     def user_params
-      params.permit()
+      params.permit(:name, :address, :city, :state, :zip, :email, :password, :password_confirmation)
     end
 end
