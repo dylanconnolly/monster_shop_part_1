43eb0690e923190a3c010550300e318cfa97c111
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-10 12:56:56 -0700

    Add validation for :percent_off to validate it is a number between 0 and 101

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,6 +2,8 @@ class Coupon < ApplicationRecord
   validates :name, uniqueness: true, presence: true
   validates :code, uniqueness: true, presence: true
   validates_presence_of :percent_off
+  validates :percent_off, numericality: { greater_than: 0, less_than_or_equal_to: 100}
+
 
   belongs_to :merchant
 end
