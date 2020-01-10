872e3687cb4671de19e50319e9077cd0d645653c
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-10 12:20:16 -0700

    Add timestamps to coupons table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200110185229) do
+ActiveRecord::Schema.define(version: 20200110191923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20200110185229) do
     t.string "code"
     t.integer "percent_off"
     t.bigint "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
   end
 
