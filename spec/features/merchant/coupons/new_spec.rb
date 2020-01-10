348afd3c1c955fb39c0de7597742b5074ce16294
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-10 11:21:13 -0700

    Create test for link to manage coupons on merchant dashboard

---
+++ spec/features/merchant/coupons/new_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "as a merchant admin" do
+  before :each do
+    @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+    @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+
+    @merchant_admin = @meg.users.create!(name: "Merchant Admin", address: "1230 East Street", city: "Boulder", state: "CO", zip: 98273, email: "merchant_admin@merchant_admin.com", password: "merchant_admin", password_confirmation: "merchant_admin", role: 2)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
+  end
+
+  it "I see a link to manage coupons from my dashboard" do
+
+    visit '/merchant'
+
+    click_link("Manage Coupons")
+
+    expect(current_path).to eq("/merchant/coupons")
+  end
+end
