1d6abbab2e1055d218f879af56b3dccaad9b941d
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-10 12:12:51 -0700

    Add create controller action (no functionality yet)

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -1,10 +1,14 @@
 class Merchant::CouponsController < Merchant::BaseController
 
   def new
-    
+    @coupon = current_user.merchant.coupons.new
   end
 
   def index
+    @coupons = Coupon.all
+  end
 
+  def create
+    
   end
 end
