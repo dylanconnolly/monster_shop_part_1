449398eef2a9125037c4c415e5934ca004d73c92
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-10 11:30:45 -0700

    Add coupons resource under merchant namespace

--- config/routes.rb
+++ config/routes.rb
@@ -62,9 +62,8 @@ Rails.application.routes.draw do
     get '/orders/:id', to: 'orders#show'
     get '/:merchant_id/items/new', to: "items#new"
     post '/:merchant_id/items', to: "items#create"
-    # get '/:merchant_id/:items_id/edit', to: "items#edit"
-    # patch '/:merchant_id/:items_id', to: "items#update"
     get '/orders/:order_id/item_orders/:item_order_id/fulfill', to: 'item_orders#fulfill'
+    resources :coupons
   end
 
   namespace :admin do
