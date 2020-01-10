a408ff6022a8f0f19f4c5585f0f536b2f744439f
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-10 11:48:06 -0700

    Create coupon model spec and test validations

---
+++ spec/models/coupon_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Coupon do
+  describe "validations" do
+    it {should validate_presence of :name}
+    it {should validate_presence of :code}
+    it {should validate_presence of :percent_off}
+    it {should validate_uniqueness_of :name}
+    it {should validate_uniqueness_of :code}
+  end
+end
