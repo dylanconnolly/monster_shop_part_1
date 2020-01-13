eb136cf05ac2c77e3d225e8b30130c01612a70d3
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-13 16:57:34 -0700

    Update user_coupon controller action 'create' to use find_by_code instead of where

--- app/controllers/user_coupon_controller.rb
+++ app/controllers/user_coupon_controller.rb
@@ -1,7 +1,7 @@
 class UserCouponController < ApplicationController
 
   def create
-    if Coupon.where(code: params[:coupon_code]) != []
+    if Coupon.find_by_code(params[:coupon_code]) != nil
       session[:coupon] = params[:coupon_code]
       flash[:success] = "Coupon code applied!"
     else
