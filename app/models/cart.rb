dc97310c0b3c62c9902b2530435c2ccf93093ee7
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-13 17:04:17 -0700

    Update all instances of item.price to be item.price / 100 since price will be stored as cents

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -23,24 +23,26 @@ class Cart
   end
 
   def subtotal(item)
-    (item.price/100) * @contents[item.id.to_s]
+    (item.price.to_f/100) * @contents[item.id.to_s]
   end
 
   def total
     @contents.sum do |item_id,quantity|
-      ((Item.find(item_id).price)/100) * quantity
+      ((Item.find(item_id).price.to_f)/100) * quantity
     end
   end
 
   def discounted_total(coupon_code)
     coupon = Coupon.find_by_code(coupon_code)
-    items.sum do |item, quantity|
-      if item.merchant == coupon.merchant
-        item.percent_discount(coupon.percent_off) * quantity
-      else
-        item.price * quantity
-      end
-    end/100
+    total_cost = items.sum do |item, quantity|
+                  if item.merchant == coupon.merchant
+                    item.percent_discount(coupon.percent_off) * quantity
+                  else
+                    item.price * quantity
+                  end
+                end
+
+    total_cost.to_f / 100
   end
   #
   # def add_quantity(item_id)
