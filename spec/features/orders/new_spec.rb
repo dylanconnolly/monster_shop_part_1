fb22efc2b364bbe39d1857b474e8867c7b2009b7
Dylan Connolly <dylanconnolly145@gmail.com>
2020-01-13 17:08:42 -0700

    Update instances of price to be price/100. Update test item creation to treat price as cents

--- spec/features/orders/new_spec.rb
+++ spec/features/orders/new_spec.rb
@@ -3,9 +3,9 @@ RSpec.describe("New Order Page") do
     before(:each) do
       @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
       @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
-      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
+      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 10000, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 2000, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
+      @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 200, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
 
       visit "/items/#{@paper.id}"
       click_on "Add To Cart"
@@ -24,7 +24,7 @@ RSpec.describe("New Order Page") do
       within "#order-item-#{@tire.id}" do
         expect(page).to have_link(@tire.name)
         expect(page).to have_link("#{@tire.merchant.name}")
-        expect(page).to have_content("$#{@tire.price}")
+        expect(page).to have_content("$#{@tire.price/100}")
         expect(page).to have_content("1")
         expect(page).to have_content("$100")
       end
@@ -32,7 +32,7 @@ RSpec.describe("New Order Page") do
       within "#order-item-#{@paper.id}" do
         expect(page).to have_link(@paper.name)
         expect(page).to have_link("#{@paper.merchant.name}")
-        expect(page).to have_content("$#{@paper.price}")
+        expect(page).to have_content("$#{@paper.price/100}")
         expect(page).to have_content("2")
         expect(page).to have_content("$40")
       end
@@ -40,7 +40,7 @@ RSpec.describe("New Order Page") do
       within "#order-item-#{@pencil.id}" do
         expect(page).to have_link(@pencil.name)
         expect(page).to have_link("#{@pencil.merchant.name}")
-        expect(page).to have_content("$#{@pencil.price}")
+        expect(page).to have_content("$#{@pencil.price/100}")
         expect(page).to have_content("1")
         expect(page).to have_content("$2")
       end
